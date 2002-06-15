4b591e55fe449f91f70878212d08793d69f9d48b
brian d foy <brian.d.foy@gmail.com>
2002-06-15 05:27:31 +0000

    * this is the first version of the module to actually do something. it can read a file, extract MP3 attributes, and create an object from that
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@130 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Item.pm
+++ lib/Item.pm
@@ -1,15 +1,159 @@
 # $Id$
 package MacOSX::iTunes::Item;
 
+use MP3::Info qw(get_mp3tag);
+
+=head1 NAME
+
+MacOSX::iTunes::Item
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4 
+
+=item new( FILE )
+
+Creates a new item from the given file name.
+
+=cut
+
 sub new
 	{
 	my $class = shift;
+	my $file  = shift;
 	
-	my $self = {};
+	return unless -e $file;
+	
+	my $tag  = MP3::Info::get_mp3tag( $file );
+	my $info = MP3::Info::get_mp3info( $file );
+	# XXX: convert to an absolute path, if necessary
+	
+	# XXX: return unless it's an MP3 file
+	
+	# XXX: extract info from MP3 file
+	
+	my $self = {
+		title    => $tag->{TITLE},
+		genre    => $tag->{GENRE},
+		seconds  => $info->{SECS},
+		file     => $file,
+		artist   => $tag->{ARTIST},
+		_tag     => $tag,
+		_info    => $info,
+		};
 	
 	bless $self, $class;
 	
 	return $self;
 	}
 
+=item title
+
+Return the title of the item
+
+=cut
+
+sub title
+	{
+	my $self = shift;
+	
+	$self->{title};
+	}
+
+=item seconds
+
+Return the length, in seconds, of the item
+
+=cut
+
+sub seconds
+	{
+	my $self = shift;
+	
+	$self->{seconds};
+	}
+	
+=item genre
+
+Return the genre of the song
+
+=cut
+
+sub genre
+	{
+	my $self = shift;
+	
+	$self->{genre};
+	}
+	
+=item file
+
+Return the filename of the item
+
+=cut
+
+sub file
+	{
+	my $self = shift;
+	
+	$self->{file};
+	}
+	
+=item artist
+
+Return the artist of the item
+
+=cut
+
+sub artist
+	{
+	my $self = shift;
+	
+	$self->{artist};
+	}
+
+=item as_string
+
+Return a string representation of the item
+
+=cut
+
+sub as_string
+	{
+	my $self = shift;
+	
+	return <<"STRING";
+FILE    $$self{file}
+TITLE   $$self{title}
+GENRE   $$self{genre}
+ARTIST  $$self{artist}
+TIME    $$self{seconds} seconds
+
+STRING
+	}
+
 "See why 1984 won't be like 1984";
+
+=back
+
+=head1 SEE ALSO
+
+L<MacOSX::iTunes>, L<MacOSX::iTunes::Playlist>, L<MP3::Info>
+
+=head1 TO DO
+
+* everything - the list of things already done is much shorter.
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+Copyright 2002, brian d foy <bdfoy@cpan.org>
+
+You may redistribute this under the same terms as Perl.
+
+=cut
