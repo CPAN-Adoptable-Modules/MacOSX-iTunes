34c7d508654a11a1c15869a4fe59704a34873cb1
brian d foy <brian.d.foy@gmail.com>
2003-01-03 20:17:26 +0000

    * updated duration value for shorter version of mp3/The_Wee_Kirkcudbright_Centipede.mp3 test mp3
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@786 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- t/lib/test_data.pl
+++ t/lib/test_data.pl
@@ -7,6 +7,6 @@ $Test_mp3      = 'mp3/The_Wee_Kirkcudbright_Centipede.mp3';
 $Title         = 'The Wee Kirkcudbright Centipede';
 $Genre         = '';
 $Artist        = 'The Tappan Sisters';
-$Time          = 186.82775;
+$Time          = 20.245;
 
 $Track_name    = 'The Wee Kirkcudbright Centipede';
